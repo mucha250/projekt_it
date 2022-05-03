576a0cad08371efaff6418f5117d16a7931dc009
SillenTi <101140200+SillenTi@users.noreply.github.com>
2022-05-03 23:07:00 +0200

    Add files via upload

--- index/scripts_users.js
+++ index/scripts_users.js
@@ -1,31 +1,230 @@
 //import axios from "axios";
 
-
 const laptopy = async () => {
-    return await axios.get("http://localhost:8081/getitem").then((response) => {
+  return await axios.get("http://localhost:8081/getitem").then((response) => {
     console.log(response);
     return response;
-    });
-}
+  });
+};
 
 const dane = await laptopy();
 console.log(`dane ${dane}`);
 var sprzet = dane.data;
 
 // Get the app element
-var app = document.querySelector('#app');
+var app = document.querySelector("#app");
 
 // Create markup
-app.innerHTML = '<ul>' + sprzet.map(function (sprzet) {
-	
-    return '<li>' + sprzet.id +" " + sprzet.nazwa_w_domenie + " " + sprzet.typ_ram + " " + '</li>';
-}).join('') + '</ul>';
+app.innerHTML =
+  "<tr>" +
+  sprzet
+    .map(function (sprzet) {
+      return (
+        '<table id="customers">' +
+        "<tr>" +
+        "<th>" +
+        "Nazwa sprzętu" +
+        "</th>" +
+        "<th>" +
+        "ID" +
+        "</th>" +
+        "<th>" +
+        "Nazwa w domenie" +
+        "</th>" +
+        "<th>" +
+        "Typ RAM" +
+        "</th>" +
+        "<th>" +
+        "Rozmiar RAM" +
+        "</th>" +
+        "<th>" +
+        "Typ Dysku" +
+        "</th>" +
+        "<th>" +
+        "Rozmiar RAM" +
+        "</th>" +
+        "<th>" +
+        "Napęd" +
+        "</th>" +
+        "<th>" +
+        "Ethernet" +
+        "</th>" +
+        "<th>" +
+        "Ilość USB" +
+        "</th>" +
+        "</tr>" +
+        "<tr>" +
+        "<td>" +
+        "Laptop" +
+        "</td>" +
+        "<td>" +
+        sprzet.id +
+        "</td>" +
+        "<td>" +
+        sprzet.nazwa_w_domenie +
+        "</td>" +
+        "<td>" +
+        sprzet.typ_ram +
+        "</td>" +
+        "<td>" +
+        sprzet.rozmiar_ram +
+        "</td>" +
+        "<td>" +
+        sprzet.typ_dysku +
+        "</td>" +
+        "<td>" +
+        sprzet.rozmiar_dysku +
+        "</td>" +
+        "<td>" +
+        sprzet.naped +
+        "</td>" +
+        "<td>" +
+        sprzet.ethernet +
+        "</td>" +
+        "<td>" +
+        sprzet.ilosc_usb +
+        "</td>" +
+        "</tr>" +
+        "<tr>" +
+        "<th>" +
+        "Nazwa sprzętu" +
+        "</th>" +
+        "<th>" +
+        "ID" +
+        "</th>" +
+        "<th>" +
+        "Nazwa w domenie" +
+        "</th>" +
+        "<th>" +
+        "Typ RAM" +
+        "</th>" +
+        "<th>" +
+        "Rozmiar RAM" +
+        "</th>" +
+        "<th>" +
+        "Typ Dysku" +
+        "</th>" +
+        "<th>" +
+        "Rozmiar RAM" +
+        "</th>" +
+        "<th>" +
+        "Napęd" +
+        "</th>" +
+        "<th>" +
+        "Ethernet" +
+        "</th>" +
+        "<th>" +
+        "Ilość USB" +
+        "</th>" +
+        "</tr>" +
+        "<tr>" +
+        "<td>" +
+        "Telefon" +
+        "</td>" +
+        "<td>" +
+        sprzet.id +
+        "</td>" +
+        "<td>" +
+        sprzet.nazwa_w_domenie +
+        "</td>" +
+        "<td>" +
+        sprzet.typ_ram +
+        "</td>" +
+        "<td>" +
+        sprzet.rozmiar_ram +
+        "</td>" +
+        "<td>" +
+        sprzet.typ_dysku +
+        "</td>" +
+        "<td>" +
+        sprzet.rozmiar_dysku +
+        "</td>" +
+        "<td>" +
+        sprzet.naped +
+        "</td>" +
+        "<td>" +
+        sprzet.ethernet +
+        "</td>" +
+        "<td>" +
+        sprzet.ilosc_usb +
+        "</td>" +
+        "</tr>" +
+        "<tr>" +
+        "<th>" +
+        "Nazwa sprzętu" +
+        "</th>" +
+        "<th>" +
+        "ID" +
+        "</th>" +
+        "<th>" +
+        "Nazwa w domenie" +
+        "</th>" +
+        "<th>" +
+        "Typ RAM" +
+        "</th>" +
+        "<th>" +
+        "Rozmiar RAM" +
+        "</th>" +
+        "<th>" +
+        "Typ Dysku" +
+        "</th>" +
+        "<th>" +
+        "Rozmiar RAM" +
+        "</th>" +
+        "<th>" +
+        "Napęd" +
+        "</th>" +
+        "<th>" +
+        "Ethernet" +
+        "</th>" +
+        "<th>" +
+        "Ilość USB" +
+        "</th>" +
+        "</tr>" +
+        "<tr>" +
+        "<td>" +
+        "Monitor" +
+        "</td>" +
+        "<td>" +
+        sprzet.id +
+        "</td>" +
+        "<td>" +
+        sprzet.nazwa_w_domenie +
+        "</td>" +
+        "<td>" +
+        sprzet.typ_ram +
+        "</td>" +
+        "<td>" +
+        sprzet.rozmiar_ram +
+        "</td>" +
+        "<td>" +
+        sprzet.typ_dysku +
+        "</td>" +
+        "<td>" +
+        sprzet.rozmiar_dysku +
+        "</td>" +
+        "<td>" +
+        sprzet.naped +
+        "</td>" +
+        "<td>" +
+        sprzet.ethernet +
+        "</td>" +
+        "<td>" +
+        sprzet.ilosc_usb +
+        "</td>" +
+        "</tr>"
+      );
+    })
+    .join("") +
+  "</tr>" +
+  "</table>";
 
-
-
-
-
-
-
-
-
+const pracownicyCsv = async () => {
+  return await axios
+    .get("http://localhost:8081/getPracownicyCsv")
+    .then((response) => {
+      console.log(response);
+      return response;
+    });
+};
+pracownicyCsv();
